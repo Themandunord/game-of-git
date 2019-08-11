76b715954155a3d39247f53abc84d8f668b1b398
MikingTheViking <kingmi123@gmail.com>
2019-08-11 10:27:30 -0400

    WIP

--- services/git-service/src/utilities/gitHubGql.ts
+++ services/git-service/src/utilities/gitHubGql.ts
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import config from '../config';
 
+/**
+ * Wrapper to easily execute a GitHub GraphQL GQL call
+ *
+ * @param key
+ * @param data
+ */
 export const gitHubGql = async (key: string, data: any) => {
 	return axios.post(
 		config.GITHUB_GRAPHQL_URL,
